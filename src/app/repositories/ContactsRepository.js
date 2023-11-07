6a4c436af23b9d31f309cc0b2efafa6862dcf780
Victor Oliveira <vggaia10@gmail.com>
2023-11-07 20:22:46 -0300

    create findAll method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,6 +1,16 @@
+const { uuid } = require('uuidv4');
+
+const contacts = [{
+  id: uuid(),
+  name: 'Victor',
+  email: 'vggaia@hotmail.com',
+  phone: '82999222520',
+  category: uuid(),
+}];
+
 class ContatcsRepository {
   findAll() {
-
+    return contacts;
   }
 }
 
