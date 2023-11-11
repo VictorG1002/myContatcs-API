dd644a5d688b5a516affc2353a35a10c40958377
Victor Oliveira <vggaia10@gmail.com>
2023-11-11 17:44:13 -0300

    add create method

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -21,8 +21,25 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // create one register
+    const { name , email , phone, category_id} = request.body
+
+    if(!name) {
+      response.status(400).json({error: 'Name is required'})
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email)
+
+    if (contactExists) {
+      response.status(400).json({error: 'This e-mail is already been taken'})
+    }
+
+    const contact = await ContactsRepository.create({
+      name , email , phone, category_id
+    })
+
+    response.json(contact)
   }
 
   update() {
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -42,6 +42,22 @@ class ContatcsRepository {
       resolve()
     })
   }
+
+  create({name , email , phone, category_id}) {
+    return new Promise((resolve) => {
+    const newContact = {
+      id: v4(),
+      name,
+      email,
+      phone,
+      category_id,
+    }
+
+    contacts.push(newContact)
+
+    resolve(newContact)
+    })
+  }
 }
 
 module.exports = new ContatcsRepository();
