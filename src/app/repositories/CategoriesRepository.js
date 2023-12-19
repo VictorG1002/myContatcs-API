4ec092eff72e42e13e62c516ee56a05e7a7065c3
Victor Oliveira <vggaia10@gmail.com>
2023-12-19 20:01:57 -0300

    feat: ends categories module

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -1,7 +1,23 @@
 const db = require("../database/index");
 
 class CategoriesRepository {
-  findAll() {}
+  async findAll(orderBy = "ASC") {
+    const direction = orderBy.toUpperCase() === "DESC" ? "DESC" : "ASC";
+
+    const rows = await db.query(
+      `SELECT * FROM categories ORDER BY name ${direction}`
+    );
+
+    return rows;
+  }
+
+  async findById(id) {
+    const [row] = await db.query("SELECT * FROM categories WHERE  id = $1", [
+      id,
+    ]);
+
+    return row;
+  }
 
   async create({ name }) {
     const [row] = await db.query(
@@ -12,6 +28,29 @@ class CategoriesRepository {
     );
 
     return row;
+  }
+
+  async update(id, { name }) {
+    const [row] = await db.query(
+      `
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *
+    `,
+      [name, id]
+    );
+
+    return row;
+  }
+
+
+  async delete(id) {
+    const deleteOp = await db.query("DELETE FROM categories WHERE id = $1", [
+      id,
+    ]);
+
+    return deleteOp;
   }
 }
 
